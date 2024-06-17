9b3df283b02297b486e61bf7f9d868b8a089b881
skywkie <dlunusin@gmail.com>
2024-06-17 12:50:21 +0300

    add home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,79 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import "../styles/Home.scss";
+
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat/Chat";
+import ChatNone from "../components/Chat/None/ChatNone";
+
 import { useAppSelector } from "../hooks";
+
+import Particle from "../utils/canvasParticles.ts";
 
 function Home() {
   const navigate = useNavigate();
   const { isAuth } = useAppSelector((state) => state.authSlice);
 
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
   React.useEffect(() => {
     if (!isAuth) return navigate("/auth/register");
   }, []);
 
-  return <div>Home</div>;
+  React.useLayoutEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    ctx.lineWidth = 1;
+
+    const particles: Particle[] = [];
+    for (let i = 0; i < 180; i++) {
+      particles.push(new Particle(canvas, ctx));
+    }
+
+    requestAnimationFrame(drawCanvas);
+
+    function drawCanvas() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+        for (let j = 0; j < particles.length; j++) {
+          const x1 = particles[i].x;
+          const y1 = particles[i].y;
+          const x2 = particles[j].x;
+          const y2 = particles[j].y;
+          const dx = x1 - x2;
+          const dy = y1 - y2;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+          if (distance < 150) {
+            const stepProgress = 1 / 50;
+            const opacity = -((distance - 100) * stepProgress);
+            ctx.strokeStyle = `hsla(${particles[i].deg}, 50%, 50%, ${opacity})`;
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.stroke();
+          }
+        }
+      }
+      requestAnimationFrame(drawCanvas);
+    }
+  }, []);
+
+  return (
+    <div className="home">
+      <canvas ref={canvasRef} id="background-canvas"></canvas>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<ChatNone />} />
+        <Route path="/chat/:userId" element={<Chat />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,19 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import { ErrorPage, Home, Register, Login } from "../pages";
 
 export function getRoutes(isAuth: boolean) {
-	if (isAuth) {
-		// TODO: редирект на главную страницу и вывод ошибки при заходе на страницу, которой не существует
+  if (isAuth) {
+    // TODO: редирект на главную страницу и вывод ошибки при заходе на страницу, которой не существует
     return (
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/*" element={<Home />} />
+        {/* <Route path="*" element={<ErrorPage />} /> */}
       </Routes>
     );
   } else {
     return (
+      // DEV MODE!!!
       <Routes>
-        <Route path="*" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/register" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     );
   }
